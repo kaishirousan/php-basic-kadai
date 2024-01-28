09437376c4b7827e261280961b5175b8536c2b30
kaishirousan <86172839+kaishirousan@users.noreply.github.com>
2024-01-28 16:08:57 +0900

    フォルダをアップロード　php kadai_005

---
+++ kadai_005/score.php
@@ -0,0 +1,27 @@
+
+
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$sum_total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+echo '<h1>課題005</h1>';
+echo '合計は'. $sum_total .'点です。';
+echo '<br>';
+$total_average = $sum_total / 10;
+echo '平均は' . $total_average . '点です。';
+
+?>
+
+
+
+
