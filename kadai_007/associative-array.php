4c921f6a50dcbf21cd101853edb7e70fde3bd6fe
kaishirousan <86172839+kaishirousan@users.noreply.github.com>
2024-01-28 16:15:24 +0900

    php kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,10 @@
+
+<?php 
+  $kadai007_array = ['name' => 'onion','price' => 200 , 'weight' => 160]; 
+
+ print_r($kadai007_array);
+
+?>
+
+
+<!-- localhost:8888/php-basic/kadai_007/associative-array.php -->
